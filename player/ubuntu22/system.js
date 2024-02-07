2c2a4e6292beaf4b24beb66a266ab614fdee05f5
gfpk87 <gfpk87@outlook.com>
2024-02-07 22:52:00 +0800

    Wed Feb  7 22:52:00 CST 2024 inscode

--- player/ubuntu22/system.js
+++ player/ubuntu22/system.js
@@ -1,4 +1,31 @@
+pmt("root", "/")
+
+function pmt(username, path) {
+    /*let username = "root";
+    let path = "~";*/
+    let prompt = username + " at " + path;
+    document.getElementById("prompt").innerText = prompt;
+}
+
 function runcmd() {
-    orgi = document.getElementById("cons").innerHTML;
-    document.getElementById("cons").innerHTML = orgi + " 123"
+    /*let orgi = document.getElementById("cons").innerHTML;*/
+    let command = document.querySelector('#cmd').value;
+    check(command);
+    /*document.getElementById("cons").innerHTML = orgi + " 123"*/
+}
+function simpPrint(message) {
+    /* let org = document.getElementById("cons").value; */
+
+    document.getElementById("cons").innerHTML += "<br/>"+message;
+}
+function check(cmd) {
+
+    if (cmd == "hello") {
+        simpPrint("Hello world!");
+    } else if (cmd == "") {
+        simpPrint("Psh: invalid command: blank.");
+    } else {
+        simpPrint("Psh: command not found.");
+    }
+    document.getElementById("cmd").value = ""
 }
